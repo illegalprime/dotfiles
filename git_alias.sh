bfd3e9a376d561e66ceeba57fb39308a51c10d85
Michael <themichaeleden@gmail.com>
2016-02-22 14:47:29 -0500

    standard git aliases

---
+++ git_alias.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Depends: git
+
+# Make some git configs
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
