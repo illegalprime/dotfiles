bd71070d8ca891ce340348ae4e304945cb8beffb
Michael Eden <themichaeleden@gmail.com>
2016-05-07 04:28:34 -0400

    more reliable, hardcoded username

--- cookbooks/desktop/attributes/default.rb
+++ cookbooks/desktop/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:user] = `who am i`.split[0]
-default[:home] = Dir.home
+default[:user] = "michael"
+default[:home] = File.join "/home", node[:user]
 
 default[:personal_bin] = File.join(node[:home], ".bin")
 default[:pleb_ui] = File.join(node[:personal_bin], "pleb_ui")
