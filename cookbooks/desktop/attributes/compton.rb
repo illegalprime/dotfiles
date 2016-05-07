46461a1b8452ef16924096332952ec8970165bc0
Michael Eden <themichaeleden@gmail.com>
2016-05-07 14:55:18 -0400

    cleanup and added compton config

---
+++ cookbooks/desktop/attributes/compton.rb
@@ -0,0 +1,1 @@
+default[:compton][:config] = File.join node[:home], ".config/compton.conf"
---
+++ cookbooks/desktop/recipes/compton.rb
@@ -0,0 +1,8 @@
+package "compton"
+
+# Create a simple config file
+template node[:compton][:config] do
+    user node[:user]
+    group node[:user]
+    source "compton/config.erb"
+end
