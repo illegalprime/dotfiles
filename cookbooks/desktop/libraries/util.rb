0bd6267545f24427f312cca07b82cf0e658ecec9
Michael Eden <themichaeleden@gmail.com>
2016-05-09 01:27:06 -0400

    added method to decide between installation depending on platform

--- cookbooks/desktop/libraries/util.rb
+++ cookbooks/desktop/libraries/util.rb
@@ -5,3 +5,42 @@ def directories(from, to, &props)
         directory File.join(from, diff[0..i]), &Proc.new
     end
 end
+
+def multipack pkg_name
+    # Get the real name of the package
+    names = if pkg_name.is_a? Hash
+        pkg_name[node[:platform]]
+    elsif pkg_name.is_a? String or pkg_name.is_a? Array
+        pkg_name
+    else
+        raise ArgumentError, "Must pass in a string or a hash"
+    end
+
+    # We'll only iterate through one package in this case
+    if names.is_a? String
+        names = [names]
+    end
+
+    # Install it specially if it needs it
+    for single_pkg in names
+        if single_pkg.include? ":"
+            type, name = single_pkg.split ":", 2
+            case type
+            when "recipe"
+                include_recipe recipe
+            when "aur"
+                fail if node[:platform] != "arch"
+                pacman_aur name
+            when "group"
+                fail if node[:platform] != "arch"
+                pacman_group name
+            end
+        else
+            if block_given?
+                package single_pkg, &Proc.new
+            else
+                package single_pkg
+            end
+        end
+    end
+end
