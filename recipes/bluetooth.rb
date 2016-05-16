2f1863a122f4d593229592cc7614426ba4b4b523
Michael Eden <themichaeleden@gmail.com>
2016-05-16 11:44:37 -0400

    started on bluetooth recipe

---
+++ recipes/bluetooth.rb
@@ -0,0 +1,14 @@
+[
+    "bluez",
+    "bluez-utils",
+    "blueman",
+]
+.each do |to_install|
+    multipack to_install
+end
+
+service "bluetooth" do
+    action :enable
+end
+
+node[:startup].push "blueman-applet"
