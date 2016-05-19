da145ea4723c495d2bef0bb19e1f76120af070e8
Michael Eden <themichaeleden@gmail.com>
2016-05-19 01:58:05 -0400

    installed racer and cargo for rust recipe (to be used with atom)

--- attributes/rust.rb
+++ attributes/rust.rb
@@ -1,6 +1,6 @@
-node[:ppas].push("ppa:hansjorg/rust")
-
 default[:rust][:git] = "https://github.com/rust-lang/rust.git"
 default[:rust][:src] = File.join node[:personal_bin], "rust"
 default[:rust][:stdlib] = File.join node[:rust][:src], "src"
 default[:rust][:cargo][:config] = File.join node[:home], ".cargo"
+
+node[:ppas].push "ppa:hansjorg/rust"
--- recipes/rust.rb
+++ recipes/rust.rb
@@ -1,11 +1,27 @@
-multipack({
-    "ubuntu" => "rust-stable",
-    "arch" => "rust",
- })
+[
+    {
+        "ubuntu" => "rust-stable",
+        "arch" => "rust",
+    },
+    {
+        "ubuntu" => "cargo-stable",
+        "arch" => "aur:cargo-bin",
+    },
+].each do |to_install|
+    multipack to_install
+end
 
 # Downlaod the source
 git node[:rust][:src] do
     repository node[:rust][:git]
     user node[:user]
     group node[:user]
+end
+
+# Install racer
+bash "install racer rust autocompleter" do
+    command "cargo install racer"
+    user node[:user]
+    group node[:user]
+    environment "HOME" => node[:home]
 end
