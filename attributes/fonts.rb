01226068e235de1977a2411e76117cf24c997b00
Michael Eden <themichaeleden@gmail.com>
2016-08-24 19:09:45 -0400

    added recipe to install custom fonts

---
+++ attributes/fonts.rb
@@ -0,0 +1,6 @@
+default[:fonts][:home] = File.join node[:home], ".fonts"
+
+default[:fonts][:files] = [
+    "SauceCodeProNerdFontCompleteMono.ttf",
+]
+
---
+++ recipes/fonts.rb
@@ -0,0 +1,21 @@
+directory node[:fonts][:home] do
+    owner node[:user]
+    group node[:user]
+    action :create
+end
+
+for font in node[:fonts][:files]
+    cookbook_file File.join node[:fonts][:home], font do
+        source File.join 'fonts', font
+        owner node[:user]
+        group node[:user]
+        action :create
+        notifies :run, 'bash[refresh-font-cache]', :delayed
+    end
+end
+
+bash 'refresh-font-cache' do
+    user node[:user]
+    group node[:user]
+    code 'fc-cache -f -v'
+end
