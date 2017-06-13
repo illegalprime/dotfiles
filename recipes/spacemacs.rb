1fff2a01a3d48945da2d546dade30291a956c6b2
Michael Eden <themichaeleden@gmail.com>
2017-06-12 20:18:32 -0700

    Updated spacemacs config and recipe.

--- recipes/spacemacs.rb
+++ recipes/spacemacs.rb
@@ -1,6 +1,17 @@
 multipack 'emacs'
 multipack 'opam'
 
+# configure ocaml
+bash 'configure opam' do
+  code 'opam init -y'
+  user node[:user]
+  group node[:user]
+  environment "HOME" => node[:home]
+  action :run
+  creates File.join node[:home], '.opam/config'
+end
+
+# install spacemacs
 git File.join node[:home], '.emacs.d' do
     repository 'https://github.com/syl20bnr/spacemacs'
     user node[:user]
