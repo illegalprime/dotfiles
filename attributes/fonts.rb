86239f3bf77ee5018de06fe092cccba95cd3da19
Michael Eden <themichaeleden@gmail.com>
2017-06-12 20:17:24 -0700

    Added SourceCodePro font for spacemacs.

--- attributes/fonts.rb
+++ attributes/fonts.rb
@@ -1,6 +1,13 @@
 default[:fonts][:home] = File.join node[:home], ".fonts"
 
 default[:fonts][:files] = [
-    "SauceCodeProNerdFontCompleteMono.ttf",
+    'SauceCodeProNerdFontCompleteMono.ttf',
+	'SourceCodePro-Black.ttf',
+	'SourceCodePro-Bold.ttf',
+	'SourceCodePro-ExtraLight.ttf',
+	'SourceCodePro-Light.ttf',
+	'SourceCodePro-Medium.ttf',
+	'SourceCodePro-Regular.ttf',
+	'SourceCodePro-Semibold.ttf',
 ]
 
